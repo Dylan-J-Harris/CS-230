2286008bf03647f28ef83682738e725543b5f48e
Dylan-J-Harris <djh0049@mix.wvu.edu>
2021-02-02 18:46:09 -0500

    lab2

---
+++ index.php
@@ -0,0 +1,51 @@
+<main>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p>information to be filled in</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card1">
+                <img src="images/watermelon.jpg">
+                <div class="container">
+                    <h2>Boss</h2>
+                    <p>PickleMan</p>
+                    <p>Email: email@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/watermelon2.jpg">
+                <div class="container">
+                    <h2>Boss</h2>
+                    <p>PickleMan</p>
+                    <p>Email: email@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/watermelon3.jpg">
+                <div class="container">
+                    <h2>Boss</h2>
+                    <p>PickleMan</p>
+                    <p>Email: email@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/watermelon4.jpg">
+                <div class="container">
+                    <h2>Boss</h2>
+                    <p>PickleMan</p>
+                    <p>Email: email@gmail.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
