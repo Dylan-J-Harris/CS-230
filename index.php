b910d9a7d04ea656f6d76293e912fba855f1c5c0
Dylan-J-Harris <djh0049@mix.wvu.edu>
2021-02-03 16:34:59 -0500

    Lab 2 Finished

--- index.php
+++ index.php
@@ -2,46 +2,47 @@
     <link rel="stylesheet" href="css/about.css">
 
     <div class="about">
-        <h1>About Us</h1>
-        <p>information to be filled in</p>
+        <h1>About Dylan's Watermelons</h1>
+        <p>Watermelons have been my favorite fruit since I was very young. As such, it was obvious that I should sell watermelons online. Now I'm 
+        rocking the watermelons</p>
     </div>
     <div class="row">
         <div class="column">
             <div class="card1">
                 <img src="images/watermelon.jpg">
                 <div class="container">
-                    <h2>Boss</h2>
-                    <p>PickleMan</p>
+                    <h2>Watermelon 1</h2>
+                    <p>Purchasing email below</p>
                     <p>Email: email@gmail.com</p>
                 </div>
             </div>
         </div>
         <div class="column">
-            <div class="card">
+            <div class="card2">
                 <img src="images/watermelon2.jpg">
                 <div class="container">
-                    <h2>Boss</h2>
-                    <p>PickleMan</p>
+                    <h2>Watermelon 2</h2>
+                    <p>Purchasing email below</p>
                     <p>Email: email@gmail.com</p>
                 </div>
             </div>
         </div>
         <div class="column">
-            <div class="card">
+            <div class="card3">
                 <img src="images/watermelon3.jpg">
                 <div class="container">
-                    <h2>Boss</h2>
-                    <p>PickleMan</p>
+                    <h2>Watermelon 3</h2>
+                    <p>Purchasing email below</p>
                     <p>Email: email@gmail.com</p>
                 </div>
             </div>
         </div>
         <div class="column">
-            <div class="card">
+            <div class="card4">
                 <img src="images/watermelon4.jpg">
                 <div class="container">
-                    <h2>Boss</h2>
-                    <p>PickleMan</p>
+                    <h2>Watermelon 4</h2>
+                    <p>Purchasing email below</p>
                     <p>Email: email@gmail.com</p>
                 </div>
             </div>
